d36c0659bdaf453596f54c6cfde44102199f2028
Emre Kandazoğlu <221118081@samsun.edu.tr>
2025-03-09 00:19:41 +0300

    new

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// EJS Template Engine Ayarı
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views')); // views klasörünü belirt
+
+// Middleware'ler
+app.use(express.static(path.join(__dirname, 'public'))); // Public klasörünü statik hale getir
+app.use(express.urlencoded({ extended: true })); // Form verilerini alabilmek için
+app.use(express.json()); // JSON verileri alabilmek için
+
+// Routers
+const guestRouter = require('./routes/guest');
+app.use('/', guestRouter);
+
+app.listen(3000, () => {
+	console.log('Server is running on port 3000');
+});
